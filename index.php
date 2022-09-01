5d889b8f08b52d22b970be515b6469f038ee45ab
Manuels-designs <Emmanuel.umeile@gmail.com>
2022-09-01 17:14:44 +0100

    styled departments section

--- index.php
+++ index.php
@@ -16,6 +16,8 @@
             <li><a href="about.php">About-us</a></li>
         </ul> 
       </nav>
+
+    <main>
     <header>
       <div class="hero-content">
         <div class="hero-main">
@@ -32,12 +34,32 @@
         </div>
       </div>
     </header>
-    <main>
-      <section class="departments">
-        <h2 class="dept-header">Our Departments</h2>
-
-      </section>
     </main>
+    <section class="departments">
+      <h2 class="dept-header">Our Departments</h2>
+
+      <div class="list">
+      <ul class="depts-list">
+        <li class="depts">Department 1</li>
+        <li class="depts">Department 2</li>
+        <li class="depts">Department 3</li>
+      </ul>
+      <ul class="depts-list">
+        <li class="depts">Department 4</li>
+        <li class="depts">Department 5</li>
+        <li class="depts">Department 6</li>
+      </ul>
+      <ul class="depts-list">
+        <li class="depts">Department 7</li>
+        <li class="depts">Department 8</li>
+        <li class="depts">Department 9</li>
+      </ul>
+      </div>
+
+    </section>
+    <footer>
+      
+    </footer>
 
 
 
