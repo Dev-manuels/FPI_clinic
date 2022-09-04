dc7fc8ec7679e2c9c583e19c83f2dd98512b5ad7
Manuels-designs <Emmanuel.umeile@gmail.com>
2022-09-04 17:17:00 +0100

    added register page

--- register.php
+++ register.php
@@ -13,10 +13,41 @@
         </div>
         <ul class="main-nav login-links">
             <li><a class="active" href="index.php">Home</a></li>
-            <li><a href="register.php">Register</a></li>
+            <li><a href="login.php">Login</a></li>
             <li><a href="appointment.php">Appointment</a></li>
         </ul> 
     </nav>
+    <main class="login-main">
+        <form method="post" class="form-container">
+            <div class="login-main-text">
+                <h2 class="hero-text-main">The Medical Centre</h2>
+                <h2 class="hero-text">Registration Portal</h2>
+            </div>
+            <div class="form-input-container">
+                <div class="form-input">
+                    <label for="Email">Email:</label>
+                    <input type="email" name="email" placeholder="Enter your Email">
+                </div>
+                <div class="form-input">
+                    <label for="Password">Position:</label>
+                    <input type="text" name="position" placeholder="Enter your position">
+                </div>
+
+                <input type="text" name="username" style="display: none;">
+                <div class="form-input">
+                    <label for="Password">Password:</label>
+                    <input type="password" name="password" placeholder="Enter your password">
+                </div>
+                <div class="form-submit">
+                    <input type="submit" value="Register">
+                </div>
+            </div>
+        </form>
+        
+
+    </main>
+    
+    
     
 </body>
 </html>
