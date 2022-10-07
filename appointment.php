c1d5d394e583a67c773dfae24bb02e060a7c3b4c
Manuels-designs <Emmanuel.umeile@gmail.com>
2022-10-07 01:13:26 +0100

    updated query

--- appointment.php
+++ appointment.php
@@ -14,16 +14,7 @@
     <title>Reservations</title>
 </head>
 <body>
-<nav class="nav-bar login-bar">
-        <div class="logo-container">
-          <a href="https://federalpolyilaro.edu.ng"><img class="logo" src="https://federalpolyilaro.edu.ng/images/header-logo.png" alt="FPI logo" height="70px" width="180px"></a>
-        </div>
-        <ul class="main-nav login-links">
-            <li><a class="active" href="index.php">Home</a></li>
-            <li><a href="login.php">Login</a></li>
-            <li><a href="register.php">Register</a></li>
-        </ul> 
-    </nav>
+    <?php include 'nav.php';?>
     <main class="main">
         <form method="post" class="form-container" >
             <div class="login-main-text">
