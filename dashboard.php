8fb5e4dc01346e84dcd1b87cbc9f1eae839e046c
Manuels-designs <Emmanuel.umeile@gmail.com>
2022-10-09 02:32:04 +0100

    added query and improved structure

--- dashboard.php
+++ dashboard.php
@@ -28,7 +28,7 @@
    
       <div class="main-text">
       <div style="color: red;, font-size: 2rem;"><b><?php echo $output  ?></b></div>
-        <h2 class="hero-main">Registration records</h2>
+        <h2 class="hero-main">Pending Registrations</h2>
       </div>
       <div class="table-container">
       <table class="table" border="1px">
@@ -54,7 +54,7 @@
 
         <!-- php code to read from database and display-->
         <?php 
-          $sql= "SELECT * from `student`";
+          $sql= "SELECT * from `student` ORDER BY `student`.`date` ASC";
           $result = mysqli_query($con,$sql);
           
           if($result){
@@ -84,14 +84,81 @@
               <td>'.$bloodgroup.'</td>
               <td>'.$status.'</td>
               <td>
-              <a href="update.php?updateid='.$id.'"" ><button>UPDATE</button></a>
-              <a href="delete.php?deleteid='.$id.'" ><button>DELETE</button></a>
-              <a href="done.php?doneid='.$id.'" ><button>DONE</button></a>
+              <a href="done.php?doneid='.$id.'&table=student" ><button>DONE</button></a>
               </td>
             </tr>' ;   
             }
           }
         ?>
+        </table>
+      </div>
+
+
+      <div class="main-text">
+      <div style="color: red;, font-size: 2rem;"><b><?php echo $output  ?></b></div>
+        <h2 class="hero-main">Registered Students</h2>
+      </div>
+      <div class="table-container">
+      <table class="table" border="1px">
+      <thead class="">
+        <tr>
+          <!-- <th scope="col">#</th> -->
+          <th scope="col">NAME</th>
+          <th scope="col">MATRIC NO</th>
+          <th scope="col">DOB</th>
+          <th scope="col">PARENT NAME</th>
+          <th scope="col">PARENT ROLE</th>
+          <th scope="col">PARENT NUMBER</th>
+          <th scope="col">GENOTYPE</th>
+          <th scope="col">BLOOD GROUP</th>
+          <th scope="col">REGISTATION-STATUS</th>
+          <th scope="col">OPERATIONS</th>
+        </tr>
+      </thead>
+      <tbody>
+
+
+
+
+        <!-- php code to read from database and display-->
+        <?php 
+          $sql= "SELECT * from `student` WHERE status = 'Registered' ORDER BY `student`.`date` ASC";
+          $result = mysqli_query($con,$sql);
+          
+          if($result){
+            while ($row=mysqli_fetch_assoc($result)) {
+              $id=$row['id'];
+              $name=$row['name'];
+              $matric=$row['matric'];
+              $dob=$row['dob'];
+              $parent=$row['parent'];
+              $role=$row['role'];
+              $phone=$row['phone'];
+              $genotype=$row['genotype'];
+              $bloodgroup=$row['bloodgroup'];
+              $status=$row['status'];
+              
+              
+              //<th scope="row">'.$id.'</th>
+              echo ' <tr>
+              
+              <td>'.$name.'</td>
+              <td>'.$matric.'</td>
+              <td>'.$dob.'</td>
+              <td>'.$parent.'</td>
+              <td>'.$role.'</td>
+              <td>'.$phone.'</td>
+              <td>'.$genotype.'</td>
+              <td>'.$bloodgroup.'</td>
+              <td>'.$status.'</td>
+              <td>
+              <a href="done.php?doneid='.$id.'&table=student" ><button>DONE</button></a>
+              </td>
+            </tr>' ;   
+            }
+          }
+        ?>
+        </table>
       </div>
     </main>
     
